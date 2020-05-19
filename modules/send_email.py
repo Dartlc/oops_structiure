1817f8d497eca7de981e321cb6c0b4c5f9e5fa1d
Dagwood <sudhagarnarayanan@gmail.com>
2020-05-19 11:02:47 +0530

    send email

--- modules/send_email.py
+++ modules/send_email.py
@@ -5,7 +5,7 @@ class SendEmail:
     def __init__(self):
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
-        s.login("sudhagardon62@gmail.com", "Narayanan")
+        s.login("sudhagardon62@gmail.com", "email_password")
         message = "hello"
         s.sendmail("sudhagardon62@gmail.com", "pranav12feb@gmail.com", message)
         s.quit()
